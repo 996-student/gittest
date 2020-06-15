07c659ca38dca5dad888d7e1c7ed546c247a3f01
996-student <1040925726@qq.com>
2020-06-15 10:02:27 +0800

    第三次

--- src/git/GitDemo.java
+++ src/git/GitDemo.java
@@ -4,8 +4,7 @@ public class GitDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello JAVA");
-		System.out.println("学Java真快乐");
+		System.out.println("Hello abcs");
 	}
 
 }
